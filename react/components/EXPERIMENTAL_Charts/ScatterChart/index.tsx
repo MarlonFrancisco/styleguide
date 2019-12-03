29688ffda939b2eee91f5b7b6fc53bd8106a7be7
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-12-02 21:41:25 -0300

    Update bubble chart

--- react/components/EXPERIMENTAL_Charts/ScatterChart/index.tsx
+++ react/components/EXPERIMENTAL_Charts/ScatterChart/index.tsx
@@ -6,16 +6,19 @@ import {
   Tooltip,
   Scatter,
   CartesianGrid,
-  ResponsiveContainer
+  ResponsiveContainer,
+  ZAxis
 } from 'recharts'
-import { getChartDefaultProps } from '../helpers'
+import PropTypes from 'prop-types'
+import { getChartDefaultProps, getRangeOfZAxis } from '../helpers'
 import { colors} from '../commonProps'
 
 const ScatterChart:FC<BaseChartProps> = ({
   data,
   config,
   xAxisKey,
-  yAxisKey
+  yAxisKey,
+  zAxisKey
 }) => {
   const { configs } = getChartDefaultProps(config)
   
@@ -25,11 +28,56 @@ const ScatterChart:FC<BaseChartProps> = ({
         <CartesianGrid {...configs.grid}/>
         <XAxis dataKey={xAxisKey} {...configs.xAxis} />
         <YAxis dataKey={yAxisKey} {...configs.yAxis} />
+        {zAxisKey && 
+          <ZAxis dataKey={zAxisKey} range={getRangeOfZAxis(zAxisKey, data)}/>
+        }
         <Tooltip cursor={false}/>
         <Scatter fill={colors[0]} />
       </ScatterChartBase>
     </ResponsiveContainer>
   )
+}
+
+ScatterChart.propTypes = {
+  /** The source data, in which each element is an object. */
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+  /** The config prop changes some styles of the chart. This prop should be given as an object.*/
+  config: PropTypes.shape({
+    xAxis: PropTypes.shape({
+      axisLine: PropTypes.bool,
+      tickLine: PropTypes.bool,
+      tick: PropTypes.bool,
+      hide: PropTypes.bool
+    }),
+    yAxis: PropTypes.shape({
+      axisLine: PropTypes.bool,
+      tickLine: PropTypes.bool,
+      tick: PropTypes.bool,
+      hide: PropTypes.bool
+    }), 
+    container: PropTypes.shape({
+      height: PropTypes.oneOfType(
+        [PropTypes.string, PropTypes.number]
+      ),
+      width: PropTypes.oneOfType(
+        [PropTypes.string, PropTypes.number]
+      ),
+    }),
+    grid: PropTypes.shape({
+      horizontal: PropTypes.bool,
+      vertical: PropTypes.bool,
+    })
+  }),
+
+  /** The key of x-axis which is corresponding to the data. */
+  xAxisKey: PropTypes.string.isRequired,
+
+  /** The keys or getter of a group of data which should be unique in a ScatterChart. */
+  dataKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+  
+  /** The key of y-axis which is corresponding to the data, it measures size of dot. */
+  zAxisKey: PropTypes.string.isRequired,
 }
 
 export default ScatterChart
